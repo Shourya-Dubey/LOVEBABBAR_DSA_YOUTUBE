af4c0151c61e6cc949898c1c139a393cefa4b222
Shouryadubey <Shourya.dubey1999@gmial.com>
2024-05-27 10:36:45 +0530

    ReverseArray

---
+++ lec9/ReverseArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int size){
+    int start = 0;
+    int end = size -1;
+    
+    while(start <= end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size){
+    for(int i =0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+main(){
+
+    int arr[6] = {1,2,3,4,5,6};
+    int brr[5] = {10, 4, 5, 1, 8};
+
+    reverse(arr, 6);
+    reverse(brr, 5);
+
+    printArray(arr, 6);
+    printArray(brr, 5);
+
+    return 0;
+}
