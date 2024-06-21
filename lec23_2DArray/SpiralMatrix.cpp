ed2ada7b3bb5a7b470f13a307735e6a504acf59c
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-06-22 00:00:52 +0530

    some changes

--- lec23_2DArray/SpiralMatrix.cpp
+++ lec23_2DArray/SpiralMatrix.cpp
@@ -45,11 +45,13 @@ using namespace std;
     }
 main(){
     int arr[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    cout<<"printing Normal"<<endl;
     for(int i = 0; i<3; i++){
         for(int j = 0; j<3; j++){
             cout<<arr[i][j]<<" ";
         }
     }
     cout<<endl;
+    cout<<"Printing Spiral"<<endl;
     printSpiral(arr);
 }
