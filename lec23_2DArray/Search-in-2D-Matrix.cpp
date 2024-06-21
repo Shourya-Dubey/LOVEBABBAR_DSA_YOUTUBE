c0e759df2b8d9eb978be68a9a4ff374a353e9579
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-06-22 00:11:09 +0530

    search-in-2D-Matrix

---
+++ lec23_2DArray/Search-in-2D-Matrix.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+bool isPresent(int arr[][3], int target){
+    int row = 3;
+    int col = 3;
+    int s = 0; 
+    int e = row*col - 1;
+
+    int mid = s + (e - s)/2;
+    while(s<=e){
+        int rowIndex = mid/col;
+        int colIndex = mid%col;
+        if(arr[rowIndex][colIndex] == target){
+            return 1;
+        }
+        else if (arr[rowIndex][colIndex] < target)
+        {
+            s = mid + 1;
+        }else{
+            e = mid - 1;
+        }
+        int mid = s + (e - s) / 2;
+    }
+    return 0;
+}
+
+main()
+{
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    if(isPresent(arr, 1)){
+        cout<<"Number is present"<<endl;
+    }else{
+        cout<<"Number is not Present"<<endl;
+    }
+}
