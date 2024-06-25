4c747e822e7c00793237899f7ec861b2942551aa
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-06-25 22:36:05 +0530

    address of operator

---
+++ lec25-Pointer-1/intro.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+    int num = 5;
+    cout<<num<<endl;
+
+    //Address Of Operator -> &
+    cout<<"address of num -> "<<&num<<endl;
+    return 0;
+}
