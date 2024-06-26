b3d3c21c03a9506986bc2a29da5d4720a92afc9a
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-06-26 11:38:52 +0530

    adding something

--- lec25-Pointer-1/play.cpp
+++ lec25-Pointer-1/play.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 main(){
+    int i = 5;
+    int *p = &i;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+
+    int num = 5;
+    int a = num;
+    a++;
+    cout<<num<<endl;
+
     return 0;
 }
