191936001b39b667988929f835f97484074efc2f
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-06-27 22:34:32 +0530

    value at index operator

--- lec26-Pointer-2/pointer_Array.cpp
+++ lec26-Pointer-2/pointer_Array.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 main(){
-    int arr[10];
+    int arr[10] = {2,4,6};
     cout<<"Address of  First Memory block -> "<<arr<<endl;
+    cout<<"Address of  First Memory block -> "<<&arr[0]<<endl;
+    cout<<"value at arr index -> "<<*arr<<endl;
     return 0;
 }
