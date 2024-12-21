e47bde6f56193e11e9d1784c557c1388421794e9
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-12-21 14:34:34 +0530

    Static Member && Destructor

--- let42 Opps-1/index.cpp
+++ let42 Opps-1/index.cpp
@@ -8,6 +8,11 @@ class Hero {
     public:
     char level;
     char name[7];
+    static int totalTimeLeft;
+    static int random(){
+        cout<<"Static member Function ";
+        return totalTimeLeft;
+    }
 
     Hero(){
         cout<<"Constructor is Calling"<<endl;
@@ -34,28 +39,59 @@ class Hero {
     char getLevel(){
        return this->level;
     }
+
+    //Copy Constructor
+    // Hero(Hero& temp){
+    //     cout<<"Copy Costructor Called"<<endl;
+    //     this->health = temp.health;
+    //     this->level = temp.level;
+    //     cout<<temp.getHealth()<<endl;
+    //     cout<<temp.getLevel()<<endl;
+    // }
+
+    ~Hero(){
+        cout<<"Destructor Called"<<endl;
+    }
+
 };
 
+// Static Data member
+int Hero::totalTimeLeft = 5;
+
 int main(){
-    Hero hero1;
-    hero1.setHealth(87);
-    hero1.setLevel('A');
-    cout<<"get Health with static : "<<hero1.getHealth()<<endl;
-    cout<<"get Level with static : "<<hero1.getLevel()<<endl;
-    cout<<hero1.level<<endl;
+    // Hero hero1;
+    // hero1.setHealth(87);
+    // hero1.setLevel('A');
+    // cout<<"get Health with static : "<<hero1.getHealth()<<endl;
+    // cout<<"get Level with static : "<<hero1.getLevel()<<endl;
+    // cout<<hero1.level<<endl;
 
-    Hero *hero2 = new Hero();
-    hero2->setHealth(50);
-    hero2->setLevel('B');
-    cout<<"Dynamic "<<(*hero2).getHealth()<<endl;
-    cout<<"Dynamic "<<(*hero2).getLevel()<<endl;
+    // Hero *hero2 = new Hero();
+    // hero2->setHealth(50);
+    // hero2->setLevel('B');
+    // cout<<"Dynamic "<<(*hero2).getHealth()<<endl;
+    // cout<<"Dynamic "<<(*hero2).getLevel()<<endl;
 
-    //OR
-    cout<<"With this-> "<<hero2->getHealth()<<endl;
-    cout<<"With this-> "<<hero2->getLevel()<<endl;
+    // //OR
+    // cout<<"With this-> "<<hero2->getHealth()<<endl;
+    // cout<<"With this-> "<<hero2->getLevel()<<endl;
 
-    Hero h1(10, 'C');
-    cout<<"Hero h1 : "<<h1.getHealth()<<" , "<<h1.getLevel()<<endl;
+    // Hero h1(10, 'C');
+    // cout<<"Hero h1 : "<<h1.getHealth()<<" , "<<h1.getLevel()<<endl;
+
+
+    // Hero h2(h1);
+
+
+    //Access Static Data Member
+    cout<<Hero::totalTimeLeft<<endl;
+    cout<<Hero::random()<<endl;
+    
+
+    Hero ramesh;
+    Hero *suresh = new Hero();
+
+    delete suresh;
 
     return 0;
 }
