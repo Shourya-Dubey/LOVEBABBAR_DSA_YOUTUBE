276ad1da2f50e29cb76de0756b02fe5d4fc63ec3
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-12-23 10:50:20 +0530

    hybridInheritance

--- lec43-Oops-2/Inheritance/hybridInheritance.cpp
+++ lec43-Oops-2/Inheritance/hybridInheritance.cpp
@@ -1,6 +1,52 @@
 #include<iostream>
 using namespace std;
+
+class A
+{
+    public:
+    int a;
+    int b;
+    
+    public:
+    void funcA()
+    {
+        cout<<"calling from Class A"<<endl;
+    }
+};
+
+class B : public A
+{
+    public:
+    void funcB()
+    {
+        cout<<"calling from Class B"<<endl;
+    }
+};
+
+class D
+{
+    public:
+    void funcD(){
+        cout<<"Calling form class D"<<endl;
+    }
+};
+
+class C : public A , public D
+{
+    public:
+    void funcC()
+    {
+        cout<<"calling from class C"<<endl;
+    }    
+};
+
+
 main() {
+
+    C obj;
+    obj.funcC();
+    obj.funcA();
+    obj.funcD();
 
 return 0;
 }
